3f6cee53370b3f77394b63cb08a4f42e42752a84
sandhya <sandhyapridhvi@gmail.com>
2020-08-18 23:05:37 +0530

    Commited Click Counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  render() {
+    return (
+      <div data-test="component-app">
+       <h1 data-test="counter-display"> The Counter Dispaly cuurently is {this.state.counter}</h1>
+       <button 
+       data-test="increment-button"
+       onClick={() => this.setState({ counter: this.state.counter + 1})}>
+         Increment Counter
+         </button>
+         <button 
+       data-test="decrement-button"
+       onClick={() => this.setState({ counter: this.state.counter - 1})}>
+         Decrement Counter
+         </button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+Enzyme.configure( {adapter: new EnzymeAdapter()} );
+
+const setup = (props={}, state=null) => {
+ const wrapper = shallow(<App {...props} />);
+ if(state) wrapper.setState(state);
+ return wrapper;
+}
+
+const findByAttr = (wrapper, val) => {
+ return wrapper.find(`[data-test="${val}"]`);
+}
+
+test ('renders without error', () => {
+const wrapper = setup();
+const appComponent = findByAttr(wrapper, 'component-app');
+expect(appComponent.length).toBe(1);
 });
+test ('renders increment button', () => {
+  const wrapper = setup();
+  const incrementButton = findByAttr(wrapper, 'increment-button');
+  expect(incrementButton.length).toBe(1);
+});
+test ('renders decrement button', () => {
+  const wrapper = setup();
+  const decrementButton = findByAttr(wrapper, 'decrement-button');
+  expect(decrementButton.length).toBe(1);
+});
+test ('renders counter display', () => {
+  const wrapper = setup();
+  const counterDispaly = findByAttr(wrapper, 'counter-display');
+  expect(counterDispaly.length).toBe(1);
+});
+test ('counter starts at 0', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+test ('clicking increment button increments counter display', () =>{
+ const counter = 7;
+ const wrapper =setup(null, { counter });
+ const button = findByAttr(wrapper, 'increment-button');
+ button.simulate('click');
+
+ const counterDispaly = findByAttr(wrapper, 'counter-display');
+ expect(counterDispaly.text()).toContain(counter + 1);
+
+});
+
