e91564e9b52ff3fc6d18838d3a49e9c5f261d2eb
sandhya <sandhyapridhvi@gmail.com>
2021-02-12 13:33:36 +0530

    Latest Commit

--- sample-project/src/App.js
+++ sample-project/src/App.js
@@ -1,38 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import './ModelsDropdown';
+import ModelsDropdown from './ModelsDropdown';
 //import { models } from './data';
 
 
 function App() {
-  const models = '[{"appointmentType" : "OPD","status" : "active"}, {"appointmentType" : "INP","status" : "active"}]';
-  
-  const [modelData] = useState (JSON.parse(models));
-  const mditems = modelData.length > 0 && modelData.map((item,i) =>{
-       return(
-       <option key = {i} value = {item.appointmentType}>{item.appointmentType}</option>
-  
-       )} );
   
   return (
 
     <div data-test="component-app">
-     <h1>Hello World!!!!</h1> 
-     <body>
-       
-           
-       <form>
-         <select data-test="myselect" >
-           <option>select a model</option>
-           {mditems}
-           
-           
-         </select>
-       </form>
-     </body>
+     <ModelsDropdown />
     </div>
   );
-}
+  }
 
 export default App;
